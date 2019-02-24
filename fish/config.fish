9fcd96797bd8ceb111e6ed6a59f8548b7bb448f0
Bartlomiej Stefanski <stefanskibartlomiej@gmail.com>
2019-02-24 12:05:06 +0100

    clean fishshell files

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,6 @@
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color 'blue'
-set __fish_git_prompt_color_branch 'green'
-set __fish_git_prompt_color_untrackedfiles 'cyan'
-set __fish_git_prompt_color_stagedstate 'green'
-set __fish_git_prompt_color_dirtystate 'yellow'
-set __fish_git_prompt_showuntrackedfiles  'yes'
-set __fish_git_prompt_showupstream 'verbose'
-
-# Status Chars
-set __fish_git_prompt_char_dirtystate '*'
-set __fish_git_prompt_char_untrackedfiles '?'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
-set __fish_git_prompt_char_upstream_equal '='
-set __fish_git_prompt_char_upstream_diverged '><'
+# pure prompt
+set -xU pure_color_mute (set_color 93A1A1)
+set -xU pure_color_success (set_color green)
 
 # man colors
 set -xU LESS_TERMCAP_md (printf "\e[01;31m")
