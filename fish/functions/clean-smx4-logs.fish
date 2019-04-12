b22e11cbe84dd6234ed1b6acacc3ab870cea6d15
Bartlomiej Stefanski <stefanskibartlomiej@gmail.com>
2019-04-12 13:39:48 +0200

    Update config

--- fish/functions/clean-smx4-logs.fish
+++ fish/functions/clean-smx4-logs.fish
@@ -1,5 +1,8 @@
 function clean-smx4-logs
-	cd ~/data/workspace/touk/
-sudo find . \( -iname "logstash.log*" -o -iname "karaf.log*" \) -exec rm -rf '{}' \;
-cd -
+  cd ~/data/workspace/touk/
+  set logs_size (find . \( -iname "logstash.log*" -o -iname "karaf.log*" \) -type f -exec du -ch {} + | grep total | cut -d ' ' -f 1)
+  test -n "$logs_size" || set logs_size "0 MB"
+  find . \( -iname "logstash.log*" -o -iname "karaf.log*" \) -type f -delete
+  echo Removed files size: $logs_size
+  cd -
 end
